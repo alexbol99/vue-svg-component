ead1b2e8acd15babb74e6329a92929c1668516ec
alexanderbol <alexbol99@gmail.com>
2023-11-24 15:50:33 +0200

    Example of vue svg-component with declarative and reactive svg elements

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
-<script setup>
+<script>
 import SvgComponent from "@/components/svgComponent.vue";
+import DrawPolyline from "@/components/drawPolyline.vue";
+export default {
+  data() {
+    return {
+      currentComponent: 'DrawPolyline'
+    }
+  },
+  components: {
+    SvgComponent,
+    DrawPolyline
+  }
+}
 </script>
 
 <template>
-  <svg-component></svg-component>
+  <button @click="currentComponent = 'SvgComponent'">
+    Show SvgComponent
+  </button>
+  <button @click="currentComponent = 'DrawPolyline'">
+    Show DrawPolyline
+  </button>
+<!--  <svg-component></svg-component>-->
+<!--  <draw-polyline></draw-polyline>-->
+  <component :is="currentComponent"></component>
 </template>
 
 <style scoped>
---
+++ src/components/drawPolyline.vue
@@ -0,0 +1,104 @@
+<template>
+  <div>
+    <svg
+        ref="svgContainer"
+        @click="handleClick"
+        @mousemove="handleMouseMove"
+        @keydown="handleKeyDown"
+        tabindex="0"
+        style="border: 1px solid #ccc"
+        width="400"
+        height="300"
+    >
+      <polyline :points="points"
+                  fill="none"
+                  stroke="black"
+                  stroke-width="2"
+                />
+<!--      <polyline-->
+<!--          v-for="(line, index) in lines"-->
+<!--          :key="index"-->
+<!--          :points="line.points"-->
+<!--          fill="none"-->
+<!--          stroke="black"-->
+<!--          stroke-width="2"-->
+<!--      />-->
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "drawPolyline",
+  data() {
+    return {
+      drawing: true,
+      lines: [],
+      currentLine: { points: [] },
+      lineStarted: false,
+    };
+  },
+  methods: {
+    handleClick(event) {
+      if (this.drawing) {
+        const svgContainer = this.$refs.svgContainer;
+        const point = this.getSVGPoint(event, svgContainer);
+        if (this.currentLine.points.length === 0) {
+          this.currentLine.points.push(point);
+        }
+        this.lineStarted = false;
+        console.log(this.currentLine.points.map(p => `${p.x} ${p.y}`))
+      }
+    },
+    handleMouseMove(ev) {
+      if (this.drawing) {
+        ev.preventDefault()
+        const svgContainer = this.$refs.svgContainer;
+        const currentPoint = this.getSVGPoint(ev, svgContainer)
+        const len = this.currentLine.points.length
+        if (this.lineStarted) {
+          this.currentLine.points[this.currentLine.points.length - 1] = currentPoint
+        }
+        else {
+          this.currentLine.points.push(currentPoint)
+          this.lineStarted = true
+        }
+      }
+    },
+    handleKeyDown(event) {
+      if (event.key === "Escape") {
+        this.stopDrawing();
+      }
+    },
+    getSVGPoint(event, svgContainer) {
+      const point = svgContainer.createSVGPoint();
+      point.x = event.clientX;
+      point.y = event.clientY;
+
+      return point.matrixTransform(svgContainer.getScreenCTM().inverse());
+    },
+    startDrawing() {
+      this.drawing = true;
+      this.currentLine = { points: [] };
+    },
+    stopDrawing() {
+      if (this.drawing) {
+        this.drawing = false;
+        this.lines.push(this.currentLine);
+      }
+    },
+  },
+  computed: {
+    points() {
+      return this.currentLine.points.map(p => `${p.x}, ${p.y}`).join(' ')
+    }
+  },
+  mounted() {
+    this.$refs.svgContainer.focus();
+  },
+};
+</script>
+
+<style scoped>
+/* Add your component styles here */
+</style>
